0593cd586e490015f3dc9ef81f591ca2e3d5193e
pradeepjadhav921 <pnjadhav921@gmail.com>
2025-03-03 00:35:47 +0530

    serve web page

--- src/WebDevelopment/WebDevelopment.js
+++ src/WebDevelopment/WebDevelopment.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import Hero from '../Web_componants/WebHero/WebHero';
+import Industry from '../Web_componants/industry/industry';
 import './WebDevelopment.css';
 
 const WebDevelopment = () => {
@@ -9,6 +10,7 @@ const WebDevelopment = () => {
     <div className="WebDevelopment">
       <Header />
       <Hero />
+      <Industry />
       <Footer />
     </div>
   );
---
+++ src/Web_componants/industry/industry.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './industry.css';
+
+const Industry = () => {
+    return (
+<div class="industries-section">
+  <h2>Industries We Serve</h2>
+    <div class="grid-container">
+        <div class="grid-item">Enterprise applications</div>
+        <div class="grid-item">ERP</div>
+        <div class="grid-item">Financial management</div>
+        <div class="grid-item">Supply chain management</div>
+        <div class="grid-item">Asset management</div>
+        <div class="grid-item">Fleet management</div>
+        <div class="grid-item">HR software</div>
+        <div class="grid-item">eLearning software</div>
+        <div class="grid-item">eCommerce</div>
+        <div class="grid-item">Mobile apps</div>
+        <div class="grid-item">Content management</div>
+        <div class="grid-item">Document management</div>
+        <div class="grid-item">CRM</div>
+        <div class="grid-item">Marketing & advertising</div>
+        <div class="grid-item">Web portals</div>
+        <div class="grid-item">Data analytics</div>
+    </div>
+    </div>
+    );
+};
+
+export default Industry;
