f820b003f4f508cfe37cdc1404bfda0a3206d438
Pradeej01959 <pradeep.jadhav@mcx.com>
2025-03-03 17:55:19 +0530

    technology

--- src/Web_componants/technology/technology.js
+++ src/Web_componants/technology/technology.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3, faJs } from '@fortawesome/free-brands-svg-icons';
 import './technology.css'; // Import the CSS file
 
 const Technologies = () => {
@@ -12,7 +14,9 @@ const Technologies = () => {
         const isVisible = boxTop < window.innerHeight && boxBottom >= 0;
 
         if (isVisible) {
-          box.classList.add('flip');
+          setTimeout(() => {
+            box.classList.add('flip');
+          }, 1000);
         } else {
           box.classList.remove('flip');
         }
@@ -37,23 +41,84 @@ const Technologies = () => {
 
       {/* CSS */}
       <div className="box" ref={(el) => (boxRefs.current[1] = el)}>
-        <img src="https://img.icons8.com/color/48/000000/css3.png" alt="CSS Icon" />
-        <h3>CSS</h3>
+      <p>We style with modern CSS, including Flexbox and Grid.</p>
         <div className="back">
-          <p>We style with modern CSS, including Flexbox and Grid.</p>
+          <img src="https://img.icons8.com/color/48/000000/css3.png" alt="CSS Icon" />
+        <h3>CSS</h3>
         </div>
       </div>
 
       {/* JavaScript */}
       <div className="box" ref={(el) => (boxRefs.current[2] = el)}>
+      <p>We build interactive and dynamic web applications.</p>
+        <div className="back">
         <img src="https://img.icons8.com/color/48/000000/javascript.png" alt="JavaScript Icon" />
         <h3>JavaScript</h3>
-        <div className="back">
-          <p>We build interactive and dynamic web applications.</p>
+         
         </div>
       </div>
 
-      {/* Add more boxes as needed */}
+      {/* Frameworks */}
+      <div className="box" ref={(el) => (boxRefs.current[3] = el)}>
+        <p>We leverage powerful frameworks to streamline development.</p>
+        <div className="back">
+          <img src="https://img.icons8.com/color/48/000000/framework.png" alt="Frameworks Icon" />
+          <h3>Frameworks</h3>
+        </div>
+      </div>
+
+      {/* Responsive Grid System */}
+        <div className="box" ref={(el) => (boxRefs.current[4] = el)}>
+        <p>We ensure your layouts look great on any device.</p>
+          <div className="back">
+          <img src="https://img.icons8.com/color/48/000000/grid.png" alt="Responsive Grid Icon" />
+          <h3>Responsive Grid System</h3>
+          </div>
+        </div>
+
+        {/* Backend Languages */}
+        <div className="box" ref={(el) => (boxRefs.current[5] = el)}>
+          <p>We ensure robust and scalable backend solutions.</p>
+          <div className="back">
+            <img src="https://img.icons8.com/color/48/000000/server.png" alt="Backend Languages Icon" />
+            <h3>Backend Languages</h3>
+          </div>
+        </div>
+
+        {/* Databases */}
+          <div className="box" ref={(el) => (boxRefs.current[6] = el)}>
+          <p>We manage and optimize your data with cutting-edge database solutions.</p>
+            <div className="back">
+            <img src="https://img.icons8.com/color/48/000000/database.png" alt="Databases Icon" />
+            <h3>Databases</h3>
+            </div>
+          </div>
+          <div className="box" ref={(el) => (boxRefs.current[7] = el)}>
+                <p>We ensure your server environment is reliable and efficient.</p>
+                <div className="back">
+                  <img src="https://img.icons8.com/color/48/000000/server.png" alt="Server Environment Icon" />
+                  <h3>Server Environment</h3>
+                </div>
+          </div>
+
+                <div className="box" ref={(el) => (boxRefs.current[8] = el)}>
+                <p>We create robust and scalable APIs for seamless integration.</p>
+                  <div className="back">
+                  <img src="https://img.icons8.com/color/48/000000/api.png" alt="API Icon" />
+                  <h3>API</h3>
+                  </div>
+                </div>
+
+
+              <div className="box" ref={(el) => (boxRefs.current[9] = el)}>
+              <p>We build interactive and dynamic web applications.</p>
+              <div className="back">
+              <img src="https://img.icons8.com/color/48/000000/security-checked.png" alt="Data Security Icon" />
+              <h3>Data Security</h3>
+              </div>
+              </div>
+
+              {/* Add more boxes as needed */}
     </div>
   );
 };
