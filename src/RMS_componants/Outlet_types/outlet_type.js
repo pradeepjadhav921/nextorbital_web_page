eca94602d59030a8978c3af567d345b34cebb105
Pradeej01959 <pradeep.jadhav@mcx.com>
2025-03-10 16:17:48 +0530

    logo RMS

--- src/RMS_componants/Outlet_types/outlet_type.js
+++ src/RMS_componants/Outlet_types/outlet_type.js
@@ -3,69 +3,70 @@ import React from 'react';
 import './outlet_type.css'; // We'll create this CSS file next
 
 const OutletTypes = () => {
+
+
   return (
     <div className="outlet-container">
       <h1>Built for all types of food business</h1>
       <p>The all-in-one restaurant Management System for all types of restaurants, farmers, and food outlets</p>
 
       <div className="outlet-grid">
-        {/* Food Courts & Canteens */}
-        <div className="outlet-item">
-          <img src="/path/to/food-court-icon.png" alt="Food Courts & Canteens" />
-          <p>Food Courts & Canteens</p>
-        </div>
+          <div className="outlet-item">
+            <img src={process.env.PUBLIC_URL + '/resto/food-removebg-preview.png'} alt="Food Courts & Canteens" />
+            <p>Food Courts & Canteens</p>
+          </div>
 
-        {/* Cafe */}
+          {/* Cafe */}
         <div className="outlet-item">
-          <img src="/path/to/cafe-icon.png" alt="Cafe" />
+          <img src={process.env.PUBLIC_URL + '/resto/cafe-removebg-preview.png'} alt="Cafe" />
           <p>Cafe</p>
         </div>
 
         {/* Fine Dine */}
         <div className="outlet-item">
-          <img src="/path/to/fine-dine-icon.png" alt="Fine Dine" />
+          <img src={process.env.PUBLIC_URL + '/resto/dinner-removebg-preview.png'} alt="Fine Dine" />
           <p>Fine Dine</p>
         </div>
 
         {/* Bar & Brewery */}
         <div className="outlet-item">
-          <img src="/path/to/bar-icon.png" alt="Bar & Brewery" />
+          <img src={process.env.PUBLIC_URL + '/resto/bar-removebg-preview.png'} alt="Bar & Brewery" />
           <p>Bar & Brewery</p>
         </div>
 
         {/* Pizzeria */}
         <div className="outlet-item">
-          <img src="/path/to/pizzeria-icon.png" alt="Pizzeria" />
+          <img src={process.env.PUBLIC_URL + '/resto/pizza-removebg-preview.png'} alt="Pizzeria" />
           <p>Pizzeria</p>
         </div>
 
         {/* OSR */}
         <div className="outlet-item">
-          <img src="/path/to/osr-icon.png" alt="OSR" />
-          <p>OSR</p>
+          <img src={process.env.PUBLIC_URL + '/resto/QSR-removebg-preview.png'} alt="QSR" />
+          <p>QSR</p>
         </div>
 
         {/* Desserts */}
         <div className="outlet-item">
-          <img src="/path/to/desserts-icon.png" alt="Desserts" />
+          <img src={process.env.PUBLIC_URL + '/resto/desart-removebg-preview.png'} alt="Desserts" />
           <p>Desserts</p>
         </div>
 
         {/* Large Chains */}
         <div className="outlet-item">
-          <img src="/path/to/large-chains-icon.png" alt="Large Chains" />
+          <img src={process.env.PUBLIC_URL + '/resto/chain-removebg-preview.png'} alt="Large Chains" />
           <p>Large Chains</p>
         </div>
 
         {/* Bakery */}
         <div className="outlet-item">
-          <img src="/path/to/bakery-icon.png" alt="Bakery" />
+          <img src={process.env.PUBLIC_URL + '/resto/bekary-removebg-preview.png'} alt="Bakery" />
           <p>Bakery</p>
         </div>
 
         {/* Cloud Kitchens */}
         <div className="outlet-item">
-          <img src="/path/to/cloud-kitchen-icon.png" alt="Cloud Kitchens" />
+          <img src={process.env.PUBLIC_URL + '/resto/cluod-removebg-preview.png'} alt="Cloud Kitchens" />
           <p>Cloud Kitchens</p>
         </div>
       </div>
